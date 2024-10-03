16e1ca57858d3cf2b0d4869407b52867c7bb5a7f
kayyraa <zindhukayrabasbay@gmail.com>
2024-10-03 20:25:39 +0300

    Auto-update from local project folder

--- scripts/account.js
+++ scripts/account.js
@@ -23,6 +23,8 @@ const SubmitButton = document.getElementById("SubmitButton");
 const UsernameLabel = document.getElementById("UsernameLabel");
 
 async function CheckUserDoc() {
+    UsernameLabel.innerHTML = "@kayra";
+    return;
     const IP = fax.GetUUID();
 
     const UserDocRef = fire.doc(UsersCollection, IP);
