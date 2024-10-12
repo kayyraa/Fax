b85755cfb1032f0151101dab7e56ffa16051baa9
kayyraa <zindhukayrabasbay@gmail.com>
2024-10-12 21:33:04 +0300

    Cleared all files

--- scripts/hud.js
+++
@@ -1,9 +0,0 @@
-import * as fax from "./faxpro.js";
-
-Array.from(document.querySelectorAll("*")).forEach(HrefLink => {
-    if (HrefLink.hasAttribute("hreflink") && HrefLink.hasAttribute("href")) {
-        HrefLink.addEventListener("mouseenter", () => fax.ShowLink(HrefLink.getAttribute("href"), true));
-        HrefLink.addEventListener("mouseleave", () => fax.ShowLink(HrefLink.getAttribute("href"), false));
-        HrefLink.addEventListener("click", () => window.open(HrefLink.getAttribute("href")));
-    }
-});
